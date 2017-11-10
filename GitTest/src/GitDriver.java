a99d3ff85a8123b08edc33fc33e509a815eea06f
Timothy <Timothy@TimPC>
2017-11-10 09:37:49 -0500

    initial check-in test

---
+++ GitTest/src/GitDriver.java
@@ -0,0 +1,9 @@
+
+public class GitDriver{
+	public static void main(String args []){
+		
+		Person p = new Person("Tim", 23);
+		
+		System.out.println("Name: " + p.getName() +"\nAge: " + p.getAge());
+	} //main
+} //GitDriver
---
+++ GitTest/src/Person.java
@@ -0,0 +1,22 @@
+
+public class Person{
+
+	private String name;
+	private int age;
+	
+	public Person(String name, int age){
+		
+		this.name = name;
+		this.age = age;
+	} //Person
+	
+	public String getName(){
+		
+		return name;
+	} //getName
+	
+	public int getAge(){
+		
+		return age;
+	} //getAge
+} //Person
