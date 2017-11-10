efcb2042110cf2589c5102d2e3f78138d04a9776
Timothy <Timothy@TimPC>
2017-11-10 09:50:16 -0500

    my first change

--- GitTest/src/GitDriver.java
+++ GitTest/src/GitDriver.java
@@ -5,5 +5,8 @@ public class GitDriver{
 		Person p = new Person("Tim", 23);
 		
 		System.out.println("Name: " + p.getName() +"\nAge: " + p.getAge());
+		
+		//newly added code
+		System.out.println("MY CHANGES");
 	} //main
 } //GitDriver
